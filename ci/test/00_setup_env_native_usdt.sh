89896996d0c662e020039046b943cb68fe06be85
0xb10c <0xb10c@gmail.com>
2022-01-24 23:33:48 +0100

    wip: add bpfcc-tools as package

--- ci/test/00_setup_env_native_usdt.sh
+++ ci/test/00_setup_env_native_usdt.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_usdt
 export DOCKER_NAME_TAG=ubuntu:20.04
-export PACKAGES="python3-zmq clang llvm libc++abi-dev libc++-dev"
+export PACKAGES="python3-zmq clang llvm libc++abi-dev libc++-dev bpfcc-tools"
 export DEP_OPTS="NO_WALLET=1 CC=clang CXX='clang++ -stdlib=libc++'"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-reduce-exports CC=clang CXX='clang++ -stdlib=libc++'"
