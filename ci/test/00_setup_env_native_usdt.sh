67a5a99eb7df77f6fbbdaec1a5c6f2cb1234680a
0xb10c <0xb10c@gmail.com>
2022-01-24 14:49:54 +0100

    wip: add dev task for tracepoint testing

---
+++ ci/test/00_setup_env_native_usdt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019-2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_usdt
+export DOCKER_NAME_TAG=ubuntu:20.04
+export PACKAGES="python3-zmq clang llvm libc++abi-dev libc++-dev"
+export DEP_OPTS="NO_WALLET=1 CC=clang CXX='clang++ -stdlib=libc++'"
+export GOAL="install"
+export BITCOIN_CONFIG="--enable-reduce-exports CC=clang CXX='clang++ -stdlib=libc++'"
