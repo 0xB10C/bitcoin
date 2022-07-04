803f365f47d2f40a88d8969f0e84a6779dc5a5cb
MacroFake <falke.marco@gmail.com>
2022-07-04 13:14:44 +0200

    ci: docker

--- ci/test/00_setup_env_native_usdt.sh
+++ ci/test/00_setup_env_native_usdt.sh
@@ -6,12 +6,14 @@
 
 export LC_ALL=C.UTF-8
 
+export DOCKER_NAME_TAG="debian:bookworm"
+export CONTAINER_NAME=ci_native_usdt
 # We install an up-to-date 'bpfcc-tools' package from an untrusted PPA.
 # This can be dropped with the next Ubuntu or Debian release that includes up-to-date packages.
 # See the if-then in ci/test/04_install.sh too.
-export ADD_UNTRUSTED_BPFCC_PPA=true
 export TRACING_PACKAGES="systemtap-sdt-dev bpfcc-tools"
-export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-dev libdb5.3++-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev libqrencode-dev libsqlite3-dev $TRACING_PACKAGES"
+export PACKAGES="clang llvm libevent-dev bsdmainutils libboost-dev $TRACING_PACKAGES"
+export RUN_UNIT_TESTS=false
 export NO_DEPENDS=1
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-c++20 --enable-zmq --enable-usdt --disable-bench --disable-fuzz --with-incompatible-bdb --without-gui CPPFLAGS='-DARENA_DEBUG -DDEBUG_LOCKORDER' CC=clang CXX=clang++"
