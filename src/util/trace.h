91161d6859d345a748e5787fb4d07f597d51fe9c
0xb10c <b10c@b10c.me>
2022-11-25 19:30:18 +0100

    tracing: only prepare tracepoint args if attached
    Before this commit, we would always prepare tracepoint arguments
    regardless of the tracepoint being used or not. While we already made
    sure not to include expensive arguments in our tracepoints, this
    commit introduces gating to make sure the arguments are only prepared
    if the tracepoints are actually used. This is a win-win improvement
    to our tracing framework. For users not interested in tracing, the
    overhead is reduced to a cheap 'greater than 0' compare. As the
    semaphore-gating technique used here is available in bpftrace, bcc,
    and libbpf, users interested in tracing don't have to change their
    tracing scripts while profiting from potential future tracepoints
    passing slightly more expensive arguments. In the context of #26531's
    mempool tracepoints, one example could be passing serialized
    transactions for RBF replacements.
    
    Under the hood, the semaphore-gating works by placing a 2-byte
    semaphore in the '.probes' ELF section. The address of the semaphore
    is contained in the ELF note providing the tracepoint information
    (`readelf -n ./src/bitcoind | grep NT_STAPSDT`). Tracing toolkits
    like bpftrace, bcc, and libbpf increase the semaphore at the address
    upon attaching to the tracepoint. We only prepare the arguments and
    reach the tracepoint if the semaphore is greater than zero. The
    semaphore is decreased when detaching from the tracepoint.
    
    The implementation of the TRACEPOINT_ACTIVE macro is flexible enough
    that it can be adapted for macOS dtrace tracing (#25541). With dtrace,
    [CONTEXT]_[EVENT]_ENABLED() can be used to gate the tracepoints.
    
    This also extends the "Adding a new tracepoint" documentation to
    include information about the semaphores and updated step-by-step
    instructions on how to add a new tracepoint.

--- src/util/trace.h
+++ src/util/trace.h
@@ -11,16 +11,54 @@
 // the optional variadic macros to define tracepoints.
 #define SDT_USE_VARIADIC 1
 
+// Setting _SDT_HAS_SEMAPHORES let's systemtap (sys/sdt.h) know that we want to
+// use the optional semaphore feature for our tracepoints. This feature allows
+// us to check if something is attached to a tracepoint. We only want to prepare
+// some potentially expensive tracepoint arguments, if the tracepoint is being
+// used. Here, an expensive argument preparation could, for example, be
+// calculating a hash or serialization of a data structure.
+#define _SDT_HAS_SEMAPHORES 1
+
 #include <sys/sdt.h>
 
-// A USDT tracepoint with zero to twelve arguments.
-#define TRACEPOINT(context, event, args...) STAP_PROBEV(context, event, args)
+// Used to define a counting semaphore for a tracepoint. This semaphore is
+// automatically incremented by tracing frameworks (bpftrace, bcc, libbpf, ...)
+// upon attaching to the tracepoint and decremented when detaching. This needs
+// to be a global variable. It's placed in the '.probes' ELF section.
+#define TRACEPOINT_SEMAPHORE(context, event) \
+    unsigned short context##_##event##_semaphore __attribute__((section (".probes")))
+
+// Returns true if something is attached to the tracepoint.
+#define TRACEPOINT_ACTIVE(context, event) context##_##event##_semaphore > 0
+
+// A USDT tracepoint with zero to twelve arguments. Here, it **IS** checked that
+// the tracepoint is active before preparing its arguments. Use the
+// TRACEPOINT_UNCHECKED macro if you want to manually check that the tracepoint
+// is active.
+#define TRACEPOINT(context, event, args...) \
+    if (TRACEPOINT_ACTIVE(context, event)) \
+        STAP_PROBEV(context, event, args)
+
+// A USDT tracepoint with zero to twelve arguments. Here, it is **NOT** checked
+// that the tracepoint is active before preparing its arguments. Use this macro
+// if you want to, for example, prepare expensive tracepoint arguments over
+// multiple lines. Example usage:
+//
+//  if (TRACEPOINT_ACTIVE(context, event)) {
+//      result = slightly_expensive_calulation();
+//      TRACEPOINT_UNCHECKED(context, event, result);
+// }
+//
+#define TRACEPOINT_UNCHECKED(context, event, args...) \
+    STAP_PROBEV(context, event, args)
 
 #else
 
+#define TRACEPOINT_SEMAPHORE(context, event)
+#define TRACEPOINT_ACTIVE(context, event) false
 #define TRACEPOINT(context, event, ...)
+#define TRACEPOINT_UNCHECKED(context, event, ...)
 
-#endif
-
+#endif // ENABLE_TRACING
 
 #endif // BITCOIN_UTIL_TRACE_H
