a358bc6e6606d4c62096e650a2ebf25519fb24b8
0xb10c <github@b10c.me>
2026-09-18 01:41:37 +0200

    ipc: add capnp wrapper for interfaces::Handler
    Add handler.capnp so IPC methods can return interfaces::Handler objects
    that clients use to unsubscribe. Taken from the chain interface PR.

---
+++ src/ipc/capnp/handler-types.h
@@ -0,0 +1,11 @@
+// Copyright (c) The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit.
+
+#ifndef BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
+#define BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
+
+#include <ipc/capnp/handler.capnp.proxy.h>
+#include <mp/type-context.h>
+
+#endif // BITCOIN_IPC_CAPNP_HANDLER_TYPES_H
